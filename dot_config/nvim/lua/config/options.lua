7f0bcdd3fd9674ae9af99995fe30744384b4c143
Norconv <connorvictorica@yahoo.com>
2024-02-07 13:15:59 -0500

    Switch nvim to fish shell

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,8 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.shell = '/bin/zsh'
-vim.python3_host_prog = '/home/norconv/.pyenv/versions/pitcrew/bin/python'
+vim.opt.shell = "/usr/bin/fish"
+vim.python3_host_prog = "/home/norconv/.pyenv/versions/pitcrew/bin/python"
 local function copy(lines, _)
   require("osc52").copy(table.concat(lines, "\n"))
 end
@@ -18,8 +18,9 @@ vim.g.clipboard = {
 }
 
 vim.filetype.add({
-    extension = {
-        astro = "astro",
-        templ = "templ",
-    }
+  extension = {
+    astro = "astro",
+    templ = "templ",
+  },
 })
+
