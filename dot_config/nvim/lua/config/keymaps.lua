cbd3d69125a8b6ef06b9a1a1502843efb5c778ba
Connor Victorica <connorvictorica@yahoo.com>
2024-07-10 18:17:32 -0400

    Updated Fish and nvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
-vim.keymap.set("n", "<leader>e", function()
-  local current_file_dir = vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":p")
-  require("mini.files").open(current_file_dir, true)
-end, { desc = "Open mini.files (Directory of Current File)" })
-
-vim.keymap.set("n", "<leader>E", function()
-  require("mini.files").open(vim.uv.cwd(), true)
-end, { desc = "Open mini.files (cwd)" })
+vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
